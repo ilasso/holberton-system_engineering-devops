79f8a1813e884937377f060693266fbcb64b339d
ilasso <1204@holbertonschool.com>
2020-05-04 21:41:12 +0000

    0x15-api:task0 imports

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,10 +4,10 @@ Python script that, using this REST API,
 for a given employee ID, returns information about his/her TODO list progress.
 """
 
-import requests
-from sys import argv
-
 if __name__ == '__main__':
+
+    import requests
+    from sys import argv
 
     Qdone = 0
     Qtotal = 0
