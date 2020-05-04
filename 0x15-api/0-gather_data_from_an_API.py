67c98dc98f8b915ee67fa622fe604e09ae65e45a
ilasso <1204@holbertonschool.com>
2020-05-04 21:49:58 +0000

    0x15-api:task0 standar out string tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
             completed.append(i.get('title'))
             Qdone = Qdone + 1
         Qtotal = Qtotal + 1
-    emplheader = "Employee {} is done with task ({}/{}):".format(username,
-                                                                 Qdone, Qtotal)
-    print(emplheader)
+    emplhead = "Employee {} is done with tasks ({}/{}):".format(username,
+                                                                Qdone, Qtotal)
+    print(emplhead)
     display = "\t " + "\n\t ".join(completed)
     print(display)
