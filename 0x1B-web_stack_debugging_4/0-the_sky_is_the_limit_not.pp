6bb1183e2c87a5e3ae490561e6000b4c91ca113e
ilasso <1204@holbertonschool.com>
2020-05-28 00:21:02 -0400

    debug4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# manifest to fix fail requests
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
+}
+
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
+}
